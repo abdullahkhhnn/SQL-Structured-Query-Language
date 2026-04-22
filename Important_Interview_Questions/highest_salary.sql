4385211ac1480235974a7f998ffc12f24ac98557
Abdullah Khan <152624203+abdullahkhhnn@users.noreply.github.com>
2026-04-22 15:49:26 +0500

    Add files via upload

---
+++ Important_Interview_Questions/highest_salary.sql
@@ -0,0 +1,43 @@
+create database [Advanced SQL Questions]
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName NVARCHAR(100),
+    Salary DECIMAL(10, 2)
+);
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Salary)
+VALUES
+(1, 'Alice Smith', 70000.00),
+(2, 'Bob Johnson', 85000.00),
+(3, 'Carol White', 95000.00),
+(4, 'David Brown', 60000.00),
+(5, 'Eve Davis', 75000.00),
+(6, 'Frank Miller', 90000.00),
+(7, 'Grace Lee', 80000.00),
+(8, 'Hannah Clark', 62000.00),
+(9, 'Ivy Harris', 74000.00),
+(10, 'Jack Lewis', 98000.00);
+
+
+select * from Employees
+
+--2nd Highest Salary
+select max(salary) [Second Highest Salary]from employees 
+where salary<(select max(salary) from employees)
+
+--3rd Highest Salary
+select max(salary) [Third Highest Salary] from employees where salary<
+(select max(salary) from employees 
+where salary<(select max(salary) from employees))
+
+--CTE
+with cte as (
+select *,DENSE_RANK() over(order by salary desc) [DR] from Employees
+)
+select salary [Second Highest Salary] from cte where DR = 2
+
+with cte as (
+select *,DENSE_RANK() over(order by salary desc) [DR] from Employees
+)
+select salary [Third Highest Salary] from cte where DR = 3
